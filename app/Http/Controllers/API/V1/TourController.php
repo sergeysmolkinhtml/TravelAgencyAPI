58e29e24a595a847bccc082867e6ccc2d408ae93
Smolkin Sergiy <37virs85@gmail.com>
2023-06-11 15:32:01 +0200

    Tours Filtering and Ordering: dynamic query, unit-tests

--- app/Http/Controllers/API/V1/TourController.php
+++ app/Http/Controllers/API/V1/TourController.php
@@ -3,15 +3,33 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ToursListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
+
 class TourController extends Controller
 {
-    public function index(Travel $travel) : AnonymousResourceCollection
+    public function index(Travel $travel, ToursListRequest $request) : AnonymousResourceCollection
     {
+
         $tours = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('starting_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
+            })
             ->orderBy('starting_date')
             ->paginate();
 
